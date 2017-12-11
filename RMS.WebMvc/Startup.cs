ad48b7638b73d47b36bb3dea6eeddee6f6fb5258
Unknown <dylan.heermann@yahoo.com>
2017-12-11 11:40:57 -0500

    Fixed common errors

---
+++ RMS.WebMvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RMS.WebMvc.Startup))]
+namespace RMS.WebMvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
