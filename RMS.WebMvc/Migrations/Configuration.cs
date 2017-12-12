7bf8a0f44df2ce4d612423ebf75958c57ab0d65f
Unknown <dylan.heermann@yahoo.com>
2017-12-12 09:40:39 -0500

    About to delete Migrations and redo them

---
+++ RMS.WebMvc/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace RMS.WebMvc.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<RMS.WebMvc.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "RMS.WebMvc.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(RMS.WebMvc.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
